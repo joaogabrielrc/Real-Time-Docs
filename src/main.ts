117bcdf235176cceb5c53b7ec588fa57a0c1320e
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2023-01-24 19:55:50 -0300

    added public files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 
 const app = express();
 
-const port = process.env.PORT ?? 8000;
+const publicDir = path.join(__dirname, '..', 'public');
+app.use(express.static(publicDir));
+
+const port = process.env.PORT ?? 3000;
 const hostname = process.env.HOSTNAME ?? '127.0.0.1';
 const httpServer = http.createServer(app);
 
