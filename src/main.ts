f2722237949c657ec7d44f2fe27871853f1703b0
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2023-01-24 20:36:59 -0300

    added socket.io

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -9,7 +10,13 @@ app.use(express.static(publicDir));
 
 const port = process.env.PORT ?? 3000;
 const hostname = process.env.HOSTNAME ?? '127.0.0.1';
+
 const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', socket => {
+  console.log('a user connected', socket.id);
+});
 
 httpServer.listen(Number(port), hostname, () =>
   console.log(`Server is listening on http://${hostname}:${port}...`)
