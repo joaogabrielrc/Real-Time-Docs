677d32d8a6e6b4556c44509d0a1b1747593e56df
João Gabriel R Cardoso <joaogabriel@cnnct.black>
2023-01-25 08:27:26 -0300

    added socket communication

--- public/document.js
+++ public/document.js
@@ -1,1 +1,11 @@
 const socket = io();
+
+const textEditorElement = document.querySelector('#text-editor');
+
+textEditorElement.addEventListener('keyup', () => {
+  socket.emit('text_editor', textEditorElement.value);
+});
+
+socket.on('text_editor_clients', data => {
+  textEditorElement.value = data;
+});
--- src/socket-back.ts
+++ src/socket-back.ts
@@ -4,5 +4,9 @@ import { httpServer } from './server';
 const io = new Server(httpServer);
 
 io.on('connection', socket => {
-  console.log('a user connected', socket.id);
+  console.log('a user connected, ID:', socket.id);
+
+  socket.on('text_editor', data => {
+    socket.broadcast.emit('text_editor_clients', data);
+  });
 });
